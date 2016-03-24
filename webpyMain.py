29047f94a653fc666f503b69c42091bb7592df85
joshadambell <joshadambell@me.com>
2016-03-24 16:50:47 -0600

    Fix returning errors

--- webpyMain.py
+++ webpyMain.py
@@ -11,44 +11,56 @@ app = web.application(urls, globals())
 
 class apiLocations:
     def GET(self):
+        web.header('Content-Type', 'application/json')
         return json.dumps({'locations': [l.serialize() for l in location.locations]})
 
 class apiLocation:
     def GET(self, location_id):
         l = location.getLocation(location_id)
         if len(l) == 0:
-            return web.notfound(self)
+            return locationNotFound(location_id)
+        web.header('Content-Type', 'application/json')
         return json.dumps({'location': l[0].serialize()})
 
 class apiLocationThermostats:
     def GET(self,location_id):
         l = location.getLocation(location_id)
         if len(l) == 0:
-            return web.notfound(self)
+            return locationNotFound(location_id)
+        web.header('Content-Type', 'application/json')
         return json.dumps({'thermostats': [thermostat.serialize() for thermostat in l[0].getThermostats()]})
 
 class apiLocationThermostat:
     def GET(self,location_id, therm_id):
         l = location.getLocation(location_id)
         if len(l) == 0:
-            return web.notfound(self)
+            return locationNotFound(location_id)
         t = l[0].getThermostat(therm_id)
         if len(t) == 0:
-            return web.notfound(self)
+            return thermostatNotFound(location_id)
+        web.header('Content-Type', 'application/json')
         return json.dumps({'thermostat': t[0].serialize()})
 
     def PUT(self, location_id, therm_id):
         l = location.getLocation(location_id)
         if len(l) == 0:
-            return web.notfound(self)
+            return locationNotFound(location_id)
         t = l[0].getThermostat(therm_id)
         if len(t) == 0:
-            return web.notfound(self)
+            return thermostatNotFound(location_id)
         updatedT, err = t[0].updateTherm(json.loads(web.data()))
         if err:
-            return web.BadRequest(message=err)
+            raise web.badrequest(message=err)
         web.header('Content-Type', 'application/json')
         return json.dumps({'thermostat': updatedT.serialize()})
+
+
+# Error messages
+def locationNotFound(location_id) :
+    raise web.notfound("location=[{}] does not exist".format(location_id))
+
+def thermostatNotFound(therm_id, location_id) :
+    raise web.notfound("thermostat=[{}] in location=[{}] does not exist".format(therm_id, location_id))
 
 if __name__ == "__main__":
     app.run()
